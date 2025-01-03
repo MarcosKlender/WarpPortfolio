f86ca012f11e5efc3ac19d4583e41c593427d9e1
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2025-01-02 22:50:40 -0500

    Fully implement i18n support Add LanguageSwitch component and i18n TS files.

---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const languages = {
+    en: 'EN',
+    es: 'ES'
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        'nav.home': '/home',
+        'nav.experience': '/experience',
+        'nav.projects': '/projects',
+        'nav.contributions': '/contributions',
+        'nav.contact': '/contact'
+    },
+    es: {
+        'nav.home': '/inicio',
+        'nav.experience': '/experiencia',
+        'nav.projects': '/proyectos',
+        'nav.contributions': '/contribuciones',
+        'nav.contact': '/contacto'
+    },
+} as const;
